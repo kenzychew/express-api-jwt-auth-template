232bcc360836249b6644cc93b19185e9550f78bc
kenzychew <kenzychew@gmail.com>
2025-02-11 11:31:45 +0800

    feat: added verifytoken middleware

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 // URL suffix in controller
 router.get("/sign-token", (req, res) => {
-  // Mock user object added
   const user = {
     _id: 1,
     username: "ken",
@@ -15,6 +14,20 @@ router.get("/sign-token", (req, res) => {
   const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "2m" });
 
   res.json({ token });
+});
+
+router.post("/verify-token", (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    res.json({ payload });
+  } catch (error) {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+});
+
+router.get("/vip", verifyToken, (req, res) => {
+  res.json({ msg: "secret" });
 });
 
 module.exports = router;
