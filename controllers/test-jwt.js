e79d6fcf41b38a2292c86a2a94c883c35ad03908
kenzychew <kenzychew@gmail.com>
2025-02-11 11:02:02 +0800

    feat: complete sign token with expiry

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -1,11 +1,20 @@
 // controllers/test-jwt.js
-
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const router = express.Router();
 
 // URL suffix in controller
 router.get("/sign-token", (req, res) => {
-  res.json({ message: "You are authorized!" });
+  // Mock user object added
+  const user = {
+    _id: 1,
+    username: "ken",
+    password: "123",
+  };
+
+  const token = jwt.sign({ user }, process.env.JWT_SECRET, { expiresIn: "2m" });
+
+  res.json({ token });
 });
 
 module.exports = router;
